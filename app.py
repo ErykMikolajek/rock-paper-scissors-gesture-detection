7274cc6041a834b58c203043b57a0eb02fe02d1e
Eryk Mikołajek <mikolajek.eryk@gmail.com>
2024-04-27 23:52:48 +0200

    project structure
    copy of kinivi/hand-gesture-recognition-mediapipe work

---
+++ app.py
@@ -0,0 +1,157 @@
+import cv2
+import mediapipe as mp
+import copy
+import numpy as np
+from utils import *
+
+
+def select_mode(key, mode):
+    ret_number = -1
+    ret_mode = mode
+    if 48 <= key <= 50:  # 0 ~ 2
+        ret_number = key - 48
+    if key == 110:  # n
+        ret_mode = 0
+    if key == 107:  # k
+        ret_mode = 1
+    return ret_number, ret_mode
+
+
+if __name__ == '__main__':
+
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+
+    cap = cv2.VideoCapture(0)
+
+    hands = mp_hands.Hands(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5,
+        max_num_hands=2
+    )
+
+    fps_calc = FpsCalc(buffer_len=10)
+    mode = 0
+    use_bound_rect = True
+
+    while True:
+        fps = fps_calc.get()
+
+        # Process Key (ESC: end)
+        key = cv2.waitKey(10)
+        if key == 27:  # ESC
+            break
+        number, mode = select_mode(key, mode)
+
+        ret, image = cap.read()
+        if not ret:
+            break
+        image = cv2.flip(image, 1)
+        org_image = copy.deepcopy(image)
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+
+        if results.multi_hand_landmarks is not None:
+            for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
+                                                  results.multi_handedness):
+                # Bounding box calculation
+                bound_rect = calc_bounding_rect(org_image, hand_landmarks)
+                # Landmark calculation
+                landmark_list = calc_landmark_list(org_image, hand_landmarks)
+
+                # Conversion to relative coordinates / normalized coordinates
+                pre_processed_landmark_list = pre_process_landmark(
+                    landmark_list)
+                # Write to the dataset file
+                logging_csv(number, mode, pre_processed_landmark_list)
+
+                # Hand sign classification
+                # hand_sign_id = landmark_classifier(pre_processed_landmark_list)
+
+                # Finger gesture classification
+                finger_gesture_id = 0
+
+                # Drawing part
+                # org_image = draw_bounding_rect(use_bound_rect, org_image, bound_rect)
+                # org_image = draw_landmarks(org_image, landmark_list)
+                # org_image = draw_info_text(
+                #     org_image,
+                #     bound_rect,
+                #     handedness,
+                #     keypoint_classifier_labels[hand_sign_id],
+                #     point_history_classifier_labels[most_common_fg_id[0][0]],
+                # )
+
+        org_image = draw_info(org_image, fps, mode, number)
+
+        # Screen reflection #############################################################
+        cv2.imshow('Hand Gesture Recognition', org_image)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+# def pre_process_point_history(image, point_history):
+#     image_width, image_height = image.shape[1], image.shape[0]
+#
+#     temp_point_history = copy.deepcopy(point_history)
+#
+#     # Convert to relative coordinates
+#     base_x, base_y = 0, 0
+#     for index, point in enumerate(temp_point_history):
+#         if index == 0:
+#             base_x, base_y = point[0], point[1]
+#
+#         temp_point_history[index][0] = (temp_point_history[index][0] -
+#                                         base_x) / image_width
+#         temp_point_history[index][1] = (temp_point_history[index][1] -
+#                                         base_y) / image_height
+#
+#     # Convert to a one-dimensional list
+#     temp_point_history = list(
+#         itertools.chain.from_iterable(temp_point_history))
+#
+#     return temp_point_history
+
+
+
+    # with mp_hands.Hands(
+    #         min_detection_confidence=0.5,
+    #         min_tracking_confidence=0.5,
+    # ) as hands:
+    #     while cap.isOpened():
+    #         success, img = cap.read()
+    #         image = img.copy()
+    #         if not success:
+    #             print("Ignoring empty camera frame.")
+    #             continue
+    #         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #         image = cv2.flip(image, 1)
+    #         image.flags.writeable = False
+    #         results = hands.process(image)
+    #         image.flags.writeable = True
+    #         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    #         x, y, _ = image.shape
+    #         cv2.line(image, (y//2, 0), (y//2, x), (0, 0, 0), 5)
+    #         if results.multi_hand_landmarks:
+    #             for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+    #                 print(hand_landmarks)
+    #                 if handedness.classification[0].label == 'Left':
+    #                     cv2.rectangle(image, (0, 0), (50, 50), (255, 0, 0),  thickness=-1)
+    #                 elif handedness.classification[0].label == 'Right':
+    #                     cv2.rectangle(image, (y - 50, 0), (y, 50), (0, 0, 255), thickness=-1)
+    #                 mp_drawing.draw_landmarks(
+    #                     image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    #         cv2.imshow('MediaPipe Hands', image)
+    #         if cv2.waitKey(5) & 0xFF == ord("q"):
+    #             break
---
+++ utils.py
@@ -0,0 +1,147 @@
+from collections import deque
+import numpy as np
+import copy
+import cv2
+import itertools
+import csv
+
+
+def calc_bounding_rect(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_array = np.empty((0, 2), int)
+
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+
+        landmark_point = [np.array((landmark_x, landmark_y))]
+
+        landmark_array = np.append(landmark_array, landmark_point, axis=0)
+
+    x, y, w, h = cv2.boundingRect(landmark_array)
+
+    return [x, y, x + w, y + h]
+
+
+def calc_landmark_list(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_point = []
+
+    # Keypoint
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+        # landmark_z = landmark.z
+
+        landmark_point.append([landmark_x, landmark_y])
+
+    return landmark_point
+
+
+def pre_process_landmark(landmark_list):
+    temp_landmark_list = copy.deepcopy(landmark_list)
+
+    # Convert to relative coordinates
+    base_x, base_y = 0, 0
+    for index, landmark_point in enumerate(temp_landmark_list):
+        if index == 0:
+            base_x, base_y = landmark_point[0], landmark_point[1]
+
+        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+
+    # Convert to a one-dimensional list
+    temp_landmark_list = list(
+        itertools.chain.from_iterable(temp_landmark_list))
+
+    # Normalization
+    max_value = max(list(map(abs, temp_landmark_list)))
+
+    # normalize_ =
+
+    temp_landmark_list = list(map(lambda n: n / max_value, temp_landmark_list))
+
+    return temp_landmark_list
+
+
+def logging_csv(number, mode, landmark_list):
+    if mode == 0:
+        pass
+    if mode == 1 and (0 <= number <= 2):
+        csv_path = 'model/landmarks_dataset.csv'
+        with open(csv_path, 'a', newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([number, *landmark_list])
+
+
+def draw_landmarks(image, hand_landmarks, ):
+    # mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    # TODO: implement
+    pass
+
+
+def draw_bounding_rect(use_bound_rect, image, bound_rect):
+    if use_bound_rect:
+        # Outer rectangle
+        cv2.rectangle(image, (bound_rect[0], bound_rect[1]), (bound_rect[2], bound_rect[3]), (0, 0, 0), 1)
+    return image
+
+
+def draw_info_text(image, bound_rect, handedness, hand_sign_text, finger_gesture_text):
+    cv2.rectangle(image, (bound_rect[0], bound_rect[1]), (bound_rect[2], bound_rect[1] - 22),
+                  (0, 0, 0), -1)
+
+    info_text = handedness.classification[0].label[0:]
+    if hand_sign_text != "":
+        info_text = info_text + ':' + hand_sign_text
+    cv2.putText(image, info_text, (bound_rect[0] + 5, bound_rect[1] - 4),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv2.LINE_AA)
+
+    if finger_gesture_text != "":
+        cv2.putText(image, "Finger Gesture:" + finger_gesture_text, (10, 60),
+                   cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 4, cv2.LINE_AA)
+        cv2.putText(image, "Finger Gesture:" + finger_gesture_text, (10, 60),
+                   cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2,
+                   cv2.LINE_AA)
+
+    return image
+
+
+def draw_info(image, fps, mode, number):
+    cv2.putText(image, "FPS:" + str(fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+               1.0, (0, 0, 0), 4, cv2.LINE_AA)
+    cv2.putText(image, "FPS:" + str(fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+               1.0, (255, 255, 255), 2, cv2.LINE_AA)
+
+    # TODO: change line below
+    mode_string = ['Logging Key Point', 'Logging Point History']
+    if 1 <= mode <= 2:
+        cv2.putText(image, "MODE:" + mode_string[mode - 1], (10, 90),
+                   cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
+                   cv2.LINE_AA)
+        if 0 <= number <= 9:
+            cv2.putText(image, "NUM:" + str(number), (10, 110),
+                       cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
+                       cv2.LINE_AA)
+    return image
+
+
+class FpsCalc(object):
+    def __init__(self, buffer_len=1):
+        self._start_tick = cv2.getTickCount()
+        self._freq = 1000.0 / cv2.getTickFrequency()
+        self._diff_times = deque(maxlen=buffer_len)
+
+    def get(self):
+        current_tick = cv2.getTickCount()
+        different_time = (current_tick - self._start_tick) * self._freq
+        self._start_tick = current_tick
+
+        self._diff_times.append(different_time)
+
+        fps = 1000.0 / (sum(self._diff_times) / len(self._diff_times))
+        fps_rounded = round(fps, 2)
+
+        return fps_rounded
