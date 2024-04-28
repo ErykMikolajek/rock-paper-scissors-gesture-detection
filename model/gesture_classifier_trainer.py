54c3c69e131449d6cb73930e2c929ddedaa6a4de
Eryk Mikołajek <mikolajek.eryk@gmail.com>
2024-04-28 09:24:47 +0200

    Update gesture_classifier_trainer.py

--- model/gesture_classifier_trainer.py
+++ model/gesture_classifier_trainer.py
@@ -1,14 +1,19 @@
 import tensorflow as tf
 
+dataset = 'model/landmarks_dataset.csv'
+model_save_path = 'model/gestures_classifier'
+
+classes = ['rock', 'paper', 'scissors']
+
 class ModelSubClassing(tf.keras.Model):
     def __init__(self):
         super().__init__()
-        
+        self.num_classes = len(classes)
         self.input = tf.keras.layers.InputLayer()
         self.dense1 = tf.keras.layers.Dense(100)
         self.dense2 = tf.keras.layers.Dense(100)
         self.dense3 = tf.keras.layers.Dense(100)
-        self.output = tf.keras.layers.Dense(3)
+        self.output = tf.keras.layers.Dense(self.num_classes)
 
     def train(self, input_tensor, training=False):
         # forward pass: block 1 
